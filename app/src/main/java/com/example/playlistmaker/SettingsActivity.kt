929e658154b1381f3bbcf5dbd1e6487732cb3642
Stepan Rashevskii <s.rashevskii@tezis-doc.ru>
2026-03-25 21:28:13 +0400

    Sprint 9 (after review)

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.net.toUri
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,14 +29,14 @@ class SettingsActivity : AppCompatActivity() {
         val shareBtn = findViewById<Button>(R.id.share_app_btn)
         val shareIntent = Intent(Intent.ACTION_SEND)
         shareIntent.putExtra(Intent.EXTRA_TEXT, "http://ya.ru")
-        shareIntent.setType("text/plain")
+        shareIntent.type = "text/plain"
         shareBtn.setOnClickListener { startActivity(shareIntent) }
 
         val writeSupportBtn = findViewById<Button>(R.id.write_to_support_btn)
-        val subject = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
-        val message = "Спасибо разработчикам и разработчицам за крутое приложение!"
+        val subject = resources.getString(R.string.email_subject)
+        val message = resources.getString(R.string.email_message)
         val mailIntent = Intent(Intent.ACTION_SENDTO)
-        mailIntent.data = Uri.parse("mailto:")
+        mailIntent.data = "mailto:".toUri()
         mailIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf("rashevskii@yandex.ru"))
         mailIntent.putExtra(Intent.EXTRA_SUBJECT, subject)
         mailIntent.putExtra(Intent.EXTRA_TEXT, message)
@@ -43,7 +44,7 @@ class SettingsActivity : AppCompatActivity() {
 
         val userAgreementBtn = findViewById<Button>(R.id.user_agreement_btn)
         val agreementIntent = Intent(Intent.ACTION_VIEW)
-        agreementIntent.data = Uri.parse("https://yandex.ru/legal/practicum_offer/ru/")
+        agreementIntent.data = "https://yandex.ru/legal/practicum_offer/ru/".toUri()
         userAgreementBtn.setOnClickListener { startActivity(agreementIntent) }
     }
 }
